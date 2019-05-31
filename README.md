4b5d96b150a4310c9a0990ef0c8395a4d46d0d5c
GenTurgidson <bradford@absurd.wtf>
2019-05-31 10:42:42 -0400

    committed 4.0 alpha 1

---
+++ README.md
@@ -0,0 +1,110 @@
+# WPM
+
+### The automagical WordPress Mangler
+
+**Project Author**: Bradford Morgan White
+
+**Project Started**: 20131222
+
+**Project Updated**: 20190531
+
+**Project Name**: WordPress Mangler
+
+**Project Purpose**: WordPress automagically
+
+---
+
+### SUMMARY
+
+WPM was initially written as just a backup automation of WordPress installations. Overtime, it got extended to do more things, until a rewrite was necessary. Now, it does all the things... automagically.
+
+### CONFIGURATION
+
+WPM calls `$PREFIX/etc/wpm.conf` for those environmental options that it has.
+
+### INSTALLATION
+
+As root, just type `make install` within this archive. There is one configurable option within the make file, and that is the installation prefix. It will default to `/usr/local`. The script assumes it is running as root, and the install will set permissions accordingly. Usage information is available with `wpm --help`. There is a `make uninstall` as well if you decide you hate it, and there is a ``make reinstall` if you modify something and make the script worse.
+
+### USAGE
+
+So, `wpm --help` will show you this:
+
+```
+usage:
+  -b | --backup	 backup a wordpress installation 
+	 -n | --no-compress	 the xz or gz container won't actually be compressed 
+	 -d | --delete-old	 delete older backups 
+	 -s | --skip-uploads	 skip the uploads directory 
+	 -p | --path	 the WordPress install's location 
+
+  -c | --copy	 copy a WP install from one domain/directory to another 
+	 -o | --oldname 
+	 -n | --newname 
+	 -s | --source 
+	 -d | --destination 
+
+  -d | --delete	 delete a WP install and DB 
+	 -b | --backup	 do a backup first 
+	 -p | --path 
+
+  -s | --search	 locate all WP installs 
+	 example: wpm --find /var/www/ 
+
+  -i | --install	install wordpress 
+	 -d | --domain 
+	 -p | --path 
+
+  -m | --mangle	 find and then update all WordPress installs 
+	 -n | --no-backup	 do not backup the WP installs first (bad idea, btw)
+	 -p | --path	 the search path for installs 
+	 -s | --skip-search	 skip doing the search for installs 
+
+  -w | --password	 change a user password permanently/temporarily 
+	 -d | --duration	 duration of password change 
+	 -u | --username	 user who's password should be changed 
+	 -v | --view	 view the WP usernames 
+	 -p | --path	 location of the WP install 
+
+  -p | --permissions	 reset file/directory perms to safe values 
+	 example: wpm -p /var/www/html 
+
+  -r | --rename	 change the domain name of WP install 
+	 -o | --oldname 
+	 -n | --newname 
+	 -p | --path 
+
+  --settings	 change or view settings 
+	 example: wpm --settings list 
+	 example: wpm --settings BACKUPPATH=/var/www/backup 
+
+  -u | --update	 update a wordpress install 
+	 -b | --backup	 do a backup first 
+	 -g | --plugins	 update plugins too 
+	 -p | --path	 location of the WP install 
+
+  -v | --verify	 scan a wordpress install 
+	 -s | --scan	 scan with maldet 
+	 -m | --md5	 md5 files from install against version from wordpress.org 
+	 -b | --backup	 run a backup first 
+	 -r | --replace	 replace modified files (requires --md5) 
+	 -p | --path		 location of the WP install
+
+  -h | --help	 print this help text
+```
+
+### NOTES
+
+The `--install` function configures WordPress to use ftpsockets for `FS_METHOD`. This will require that you have an FTP server running even if it's only listening on 127.0.0.1. This same assumption is made by the `--permissions` function. Both `--install` and `--permissions` will set the user and group such that the web process is not the owner of the files, and that the web group will have read access only. This is to attempt to prevent file injection. The `--permissions` function does make permission settings backups if the `getfacl` command is available.
+
+The `--verify` function requires maldet if you wish to use the `--scan` option. The `--md5` option will download a copy of the installed WP version from WordPress.org and will md5 every file and compare. If you select the `--replace` option, any file will get replaced if a difference is detected. The `--replace` option also enables quarantining in `--scan`. It is highly recommended to only use `--replace` if you use `--backup`.
+
+---
+
+### LICENSE
+
+Copyright MMXIX, Bradford Morgan White
+
+Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
---
+++ bin/wpm.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+. 6b4178521b3f/etc/wpm/wpm.conf
+. 6b4178521b3f/lib/wpm-backup.inc
+. 6b4178521b3f/lib/wpm-copy.inc
+. 6b4178521b3f/lib/wpm-delete.inc
+. 6b4178521b3f/lib/wpm-find.inc
+. 6b4178521b3f/lib/wpm-install.inc
+. 6b4178521b3f/lib/wpm-mangle.inc
+. 6b4178521b3f/lib/wpm-password.inc
+. 6b4178521b3f/lib/wpm-permissions.inc
+. 6b4178521b3f/lib/wpm-rename.inc
+. 6b4178521b3f/lib/wpm-settings.inc
+. 6b4178521b3f/lib/wpm-update.inc
+. 6b4178521b3f/lib/wpm-verify.inc
+
+WPMVERSION="4.0"
+
+############################
+# TRY BLOCK IMPLEMENTATION #
+# SCRIPT HALTS ON FAILURE  #
+############################
+
+scream() { echo "$0: $*" >&2 | tee -a $LOGFILE; }
+die() { scream "$*"; exit 666; }
+try() { "$@" || die "cannot $*"; }
+
+#############################
+# MAKE SURE LOG FILE EXISTS #
+#############################
+
+([ -e $LOGFILE ] || try touch $LOGFILE ) && try chmod 660 $LOGFILE
+
+#################################
+# MAKE SURE THAT TEMPDIR EXISTS #
+#################################
+
+[ -d $TEMPDIR ] && chmod 770 $TEMPDIR || "$TEMPDIR does not exist"
+[ -d $TEMPDIR ] || exit 1
+
+########################################
+# MAKE SURE THAT THE BACKUPPATH EXISTS #
+########################################
+
+[ -d $BACKUPPATH ] || try mkdir -m 770 -p $BACKUPPATH
+[ -d $BACKUPPATH ] || exit 1
+
+####################################
+# SET THE REFERENCE ID FOR LOGGING #
+####################################
+
+r=`openssl rand -hex 5`
+
+###########################################
+# IF USER VARIABLES ARE NOT SET IN CONFIG #
+###########################################
+
+if [ -z $SELECTED_GROUP ]; then
+  local n=`ps -ef | grep nginx | grep -v root | grep -v grep | awk '{print $1}' | sort | uniq`
+  local h=`ps -ef | grep httpd | grep -v root | grep -v grep | awk '{print $1}' | sort | uniq`
+  local a=`ps -ef | grep apache | grep -v root | grep -v grep | awk '{print $1}' | sort | uniq`
+  [ ! -z $n ] || $SELECTED_GROUP=`id -g $n`
+  [ ! -z $h ] || $SELECTED_GROUP:=`id -g $h`
+  [ ! -z $a ] || $SELECTED_GROUP:=`id -g $a`
+  [ -z $SELECTED_GROUP ] && echo "no selected group found, please set it in  6b4178521b3f/etc/wpm/wpm.conf"
+  unset n
+  unset h
+  unset a
+fi
+
+######################
+# THE USAGE FUNCTION #
+######################
+
+function usage() {
+	printf "wpm version ${WPMVERSION}\n"
+	printf "usage:\n"
+
+	printf "  -b | --backup\t backup a wordpress installation \n"
+	printf "\t -n | --no-compress\t the xz or gz container won't actually be compressed \n"
+	printf "\t -d | --delete-old\t delete older backups \n"
+	printf "\t -s | --skip-uploads\t skip the uploads directory \n"
+	printf "\t -p | --path\t the WordPress install's location \n\n"
+
+	printf "  -c | --copy\t copy a WP install from one domain/directory to another \n"
+	printf "\t -o | --oldname \n"
+	printf "\t -n | --newname \n"
+	printf "\t -s | --source \n"
+	printf "\t -d | --destination \n\n"
+	
+	printf "  -d | --delete\t delete a WP install and DB \n"
+	printf "\t -b | --backup\t do a backup first \n"
+	printf "\t -p | --path \n\n"
+	
+	printf "  -s | --search\t locate all WP installs \n"
+	printf "\t example: wpm --find /var/www/ \n\n"
+	
+	printf " -i | --install\t install wordpress \n"
+	printf "\t -d | --domain \n"
+	printf "\t -p | --path \n\n"
+	
+	printf " -m | --mangle\t find and then update all WordPress installs \n"
+	printf "\t -n | --no-backup\t do not backup the WP installs first (bad idea, btw) \n"
+	printf "\t -p | --path\t the search path for installs \n"
+	printf "\t -s | --skip-search\t skip doing the search for installs \n\n"
+	
+	printf "  -w | --password\t change a user password permanently/temporarily \n"
+	printf "\t -d | --duration\t duration of password change \n"
+	printf "\t -u | --username\t user who's password should be changed \n"
+	printf "\t -v | --view\t view the WP usernames \n"
+	printf "\t -p | --path\t location of the WP install \n\n"
+	
+	printf "  -p | --permissions\t reset file/directory perms to safe values \n"
+	printf "\t example: wpm -p /var/www/html \n\n"
+	
+	printf "  -r | --rename\t change the domain name of WP install \n"
+	printf "\t -o | --oldname \n"
+	printf "\t -n | --newname \n"
+	printf "\t -p | --path \n\n"
+
+	printf "  --settings\t change or view settings \n"
+	printf "\t example: wpm --settings list \n"
+	printf "\t example: wpm --settings BACKUPPATH=/var/www/backup \n\n"
+	
+	printf "  -u | --update\t update a wordpress install \n"
+	printf "\t -b | --backup\t do a backup first \n"
+	printf "\t -g | --plugins\t update plugins too \n"
+	printf "\t -p | --path\t location of the WP install \n\n"
+	
+	printf "  -v | --verify\t scan a wordpress install \n"
+	printf "\t -s | --scan\t scan with maldet \n"
+	printf "\t -m | --md5\t md5 files from install against version from wordpress.org \n"
+	printf "\t -b | --backup\t run a backup first \n"
+	printf "\t -r | --replace\t replace modified files (requires --md5) \n"
+	printf "\t -p | --path\t location of the WP install \n\n"
+
+	printf "  -h | --help\t print this help text \n"
+} # END USAGE
+
+
+#################
+# LET'S DO THIS #
+#################
+
+case "$1" in
+  -r|--rename) rename "${@:2}" ;;
+  -c|--copy) copy "${@:2}" ;;
+  -i|--install) install "${@:2}" ;;
+  -u|--update) update "${@:2}" ;;
+  -p|--permissions) permissions"${@:2}" ;;
+  -d|--delete) delete "${@:2}" ;;
+  -b|--backup) backup "${@:2}" ;;
+  -w|--password) password "${@:2}" ;;
+  --settings) settings "${@:2}" ;;
+  -s|--search) search "${@:2}" ;;
+  -m|--mangle) mangle "${@:2}" ;;
+  -v|--verify) verify "${@:2}" ;;
+  -h|--help) usage ;;
+  --) break ;;
+  *) echo "$1 not implemented" ; exit 1 ;;
+esac
+
+# EOF
