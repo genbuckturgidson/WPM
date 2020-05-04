6aa6a4d5eb3a988eb5b608cd1e5a2e7fadb7132d
GenTurgidson <e9pDfrQc7!zSvg>
2020-05-04 15:55:41 -0400

    still won't skip uploads dir

--- lib/wpm-backup.sh
+++ lib/wpm-backup.sh
@@ -122,7 +122,7 @@ function backup() {
   [ `which getfacl` ] && echo "==$INSTANCEID==Permissions backup completed." | tee -a $LOGFILE
   [ `which getfacl` ] && echo "==$INSTANCEID==Restore with setfacl --restore=${$DESTDIR}/.permissions_backup.${DATETIME}" | tee -a $LOGFILE
   
-  CMD="try tar $TAROPTIONS - $EXCLUDES --transform=\"flags=r;s|^|$DATETIME/|\" --show-transformed * $TEMPDIR/$DBNAME.$DATETIME.sql | $INTERPRED $PRED"
+  CMD="try tar $TAROPTIONS - --transform=\"flags=r;s|^|$DATETIME/|\" --show-transformed * $TEMPDIR/$DBNAME.$DATETIME.sql $EXCLUDES | $INTERPRED $PRED"
   
   eval $CMD
 
